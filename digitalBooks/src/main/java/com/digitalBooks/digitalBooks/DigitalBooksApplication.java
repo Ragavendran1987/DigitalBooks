3f1764ca5d76a4575e4800300a45af7fec29aeb3
unknown <cogjava3206@iiht.tech>
2022-09-09 08:15:54 +0530

    Initial Commit

---
+++ digitalBooks/src/main/java/com/digitalBooks/digitalBooks/DigitalBooksApplication.java
@@ -0,0 +1,14 @@
+package com.digitalBooks.digitalBooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalBooksApplication.class, args);
+		System.out.println("Vlab Training");
+	}
+
+}
